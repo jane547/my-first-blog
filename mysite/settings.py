168cc5bdef6802553e86744748a120c9c7f696ea
admin <admin@admin.com>
2017-11-20 18:16:25 -0800

    adding pythonanywhere url

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'quf^d%03m%!b)aulyn7qyi1==bkxbh2)d75z2xe@c%x(ux@8rp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<your_username>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'jane547.pythonanywhere.com']
 
 
 # Application definition
